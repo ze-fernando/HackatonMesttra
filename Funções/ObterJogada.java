ae056077d9115dc9f01116ba6cb0b8390531e141
ze-fernando <zefernando.dev@gmail.com>
2024-12-07 11:45:51 -0300

    fix function

--- Funções/ObterJogada.java
+++ Funções/ObterJogada.java
@@ -1,8 +1,13 @@
 public static class ObterJogada{
     public static int[] obterJogadaUsuario(String posicoesLivres, Scanner teclado) {
-        System.out.println(posicoesLivres);
+        System.out.println("Digite dois valores seprados por espaço LINHA COLUNA: ");
         String jogada = teclado.nextLine();
-        int[] jogadaInt = converterJogadaStringParaVetorInt(jogada);
+        if(posicoesLivres.contains(jogada[0]) && posicoesLivres.contains(jogada[1])){
+            int[] jogadaInt = converterJogadaStringParaVetorInt(jogada);
+        }
+        else{
+            System.out.println("Jogada inválida");
+        }
         
         return jogadaInt;
     }
