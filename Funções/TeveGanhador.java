746d71ec113eb1fcfdb8f0b20a2f21759a64ba8e
ze-fernando <zefernando.dev@gmail.com>
2024-12-07 11:36:41 -0300

    teve ganhador

---
+++ Funções/TeveGanhador.java
@@ -0,0 +1,30 @@
+public static class TeveGanhador{
+    public static boolean teveGanhador(char[][] tabuleiro, char caractereJogador) {
+        if(teveGanhadorLinha(tabuleiro, caractereJogador) ||
+        teveGanhadorLinha(tabuleiro, caractereJogador) ||
+        teveGanhadorDiagonalPrincipal(tabuleiro, caractereJogador) ||
+        teveGanhadorDiagonalSecudanria(tabuleiro, caractereJogador)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+    public static boolean teveGanhadorLinha(char[][] tabuleiro, char caractereJogador) {
+        //
+    }
+
+    public static boolean teveGanhadorColuna(char[][] tabuleiro, char caractereJogador) {
+        //
+    }
+
+    public static boolean teveGanhadorDiagonalPrincipal(char[][] tabuleiro, char caractereJogador) {
+        //
+    }
+
+    public static boolean teveGanhadorDiagonalSecundaria(char[][] tabuleiro, char caractereJogador) {
+        //
+    }
+}
