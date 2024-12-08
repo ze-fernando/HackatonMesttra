7cb8791bf61a0ddb039c184d01e68387367bc36a
Yago Porto <yagosouz8@gmail.com>
2024-12-08 14:21:09 -0300

    adição de funções a classe "teveGanhador"

--- src/com/funcoes/TeveGanhador.java
+++ src/com/funcoes/TeveGanhador.java
@@ -1,10 +1,12 @@
 package com.funcoes;
-public static class TeveGanhador{
+
+
+public class TeveGanhador{
     public static boolean teveGanhador(char[][] tabuleiro, char caractereJogador) {
         if(teveGanhadorLinha(tabuleiro, caractereJogador) ||
         teveGanhadorLinha(tabuleiro, caractereJogador) ||
         teveGanhadorDiagonalPrincipal(tabuleiro, caractereJogador) ||
-        teveGanhadorDiagonalSecudanria(tabuleiro, caractereJogador)){
+        teveGanhadorDiagonalSecundaria(tabuleiro, caractereJogador)){
             return true;
         }
         else{
@@ -22,10 +24,14 @@ public static class TeveGanhador{
     }
 
     public static boolean teveGanhadorDiagonalPrincipal(char[][] tabuleiro, char caractereJogador) {
-        //
+        return tabuleiro[0][0] == caractereJogador &&
+        tabuleiro[1][1] == caractereJogador &&
+        tabuleiro[2][2] == caractereJogador;
     }
 
     public static boolean teveGanhadorDiagonalSecundaria(char[][] tabuleiro, char caractereJogador) {
-        //
+        return tabuleiro[0][2] == caractereJogador &&
+                tabuleiro[1][1] == caractereJogador &&
+                tabuleiro[2][0] == caractereJogador;
     }
 }
