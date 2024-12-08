33cb8172b6028afba8d978c142d1160c10ba6b91
ze-fernando <zefernando.dev@gmail.com>
2024-12-08 00:17:36 -0300

    add: char user

---
+++ src/com/funcoes/ObterCaracterUsuario.java
@@ -0,0 +1,27 @@
+package estudos_back.jogoVelha;
+import java.util.Scanner;
+
+public class ObterCaracterUsuario {
+
+    static char obterCaractereUsuario(Scanner teclado) {
+        final char[] CARACTERES_IDENTIFICADORES_ACEITOS = {'X', 'O', '0', 'U', 'C'} ;
+
+        while (true) {
+            try {
+                System.out.println("Escolha o seu caractere (X ou O):");
+
+                String entrada = teclado.nextLine().toUpperCase();
+
+                if (entrada.length() == 1 && (entrada.charAt(0) == 'X' || entrada.charAt(0) == 'O' || 
+                    entrada.charAt(0) == '0'  || entrada.charAt(0) == 'U' || entrada.charAt(0) == 'C')) {
+                    return entrada.charAt(0); // Retorna o caractere válido escolhido pelo usuário
+                }
+
+                System.out.println("Entrada inválida. Por favor, escolha um caractere válido.");
+            } catch (Exception e) {
+                System.out.println("Erro ao ler a entrada. Tente novamente.");
+                teclado.nextLine(); // Limpa a entrada em caso de erro
+            }
+        }
+    }
+}
