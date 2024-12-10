3781c4d82240d7514e76747bc24912e3859c397a
ze-fernando <zefernando.dev@gmail.com>
2024-12-10 16:57:09 -0300

    add aleander func

--- src/com/funcoes/Empate.java
+++ src/com/funcoes/Empate.java
@@ -5,7 +5,17 @@ import com.funcoes.Tabuleiro;
 public class Empate{
 
     public static void exibirEmpate(){
-        //
+        System.out.println("**********************************");
+        System.out.println("*          Ocorreu empate!       *");
+        System.out.println("**********************************");
+        System.out.println("*                                *");
+        System.out.println("*          ___     ___           *");
+        System.out.println("*         |  0|   |0  |          *");
+        System.out.println("*         |___| X |___|          *");
+        System.out.println("*                                *");
+        System.out.println("*          Placar: 0 X 0         *");
+        System.out.println("*                                *");
+        System.out.println("**********************************");
     }
 
     public static boolean teveEmpate(char[][] tabuleiro){
---
+++ src/com/funcoes/Posicoes.java
@@ -0,0 +1,19 @@
+public static class Posicoes{
+    public static String retornarPosicoesLivres(char[][] tabuleiro){
+        StringBuilder posicoesLivres = new StringBuilder();
+
+        
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro[i].length; j++) {
+                if (tabuleiro[i][j] == ' ') {
+                    if (posicoesLivres.length() > 0) {
+                        posicoesLivres.append(";");
+                    }
+                    posicoesLivres.append(i).append(j);
+                }
+            }
+        }
+
+        return posicoesLivres.toString();
+    }
+}
